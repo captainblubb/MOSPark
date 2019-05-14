03a7db0e1764071ca9e849693690dbc4fc6e1b31
Hans Fuchs <f.hanschristian@gmail.com>
2019-05-14 11:00:20 +0200

    placeholder structure with routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Main from './components/Main';
+import Login from './components/Login';
+import Profile from './components/Profile';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/login/">Login</Link>
+              </li>
+              <li>
+                <Link to="/profile/">Profile</Link>
+              </li>
+            </ul>
+          </nav>
+          <Route path="/" exact component={Main} />
+          <Route path="/login/" component={Login} />
+          <Route path="/profile/" component={Profile} />
+        </div>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
---
+++ frontend/src/components/Profile.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Profile() {
+    return (
+        <div>yo</div>
+    )
+}
+
+export default Profile;
