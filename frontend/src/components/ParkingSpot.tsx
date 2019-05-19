9cd63108d58d5c37ef73ec9d76843f0fbe05f41d
hansfuchs <f.hanschristian@gmail.com>
2019-05-19 20:35:53 +0200

    added function to select/unselect parkingSpots

--- frontend/src/components/ParkingSpot.tsx
+++ frontend/src/components/ParkingSpot.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 
-class ParkingSpot extends React.Component<{id: number, occupied: boolean, userId: number}, {occupied: boolean}> {
+class ParkingSpot extends React.Component<{id: number, occupied: boolean, userId: number}, {selected: boolean}> {
     constructor(props: {id: number, occupied: boolean, userId: number}) {
         super(props);
+        this.state = {
+            selected: false
+        };
+
+        this.toggleSelection = this.toggleSelection.bind(this);
+    }
+
+    toggleSelection(): void {
+        this.setState({
+            selected: !this.state.selected
+        })
     }
 
     render() {
         return (
-            <div>
+            <div onClick={this.toggleSelection} className={this.state.selected ? "parkingSpot selected" : "parkingSpot"}>
                 {this.props.id} - {this.props.occupied ? "true" : "false"} - {this.props.userId}
             </div>
         )
