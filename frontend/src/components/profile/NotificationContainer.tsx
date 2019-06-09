108e225dc7926ed65073acf9d9836c943fc0a1b4
hansfuchs <f.hanschristian@gmail.com>
2019-06-09 19:35:40 +0200

    finished restructure of NotificationContainer

---
+++ frontend/src/components/profile/NotificationContainer.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import Notification from "./Notification";
+import NotificationJson from "./NotificationJson";
+
+class NotificationContainer extends React.Component<
+    {
+        currentUserId: number;
+    },
+    {
+        notifications: Array<NotificationJson>;
+    }
+> {
+    constructor(props: { currentUserId: number }) {
+        super(props);
+
+        NotificationContainer.fetchNotifications = NotificationContainer.fetchNotifications.bind(
+            this
+        );
+        this.state = {
+            notifications: NotificationContainer.fetchNotifications()
+        };
+        this.renderNotifications = this.renderNotifications.bind(this);
+    }
+
+    static fetchNotifications(): Array<NotificationJson> {
+        /*
+        fetch(`http://localhost:8080/notificationsForUser/{this.props.currentUserId}`)
+            .then(result => result.json())
+            .then(notifications => this.setState({
+                notifications: JSON.parse(notifications)
+            }));
+        */
+        return JSON.parse(
+            '[{"id": 0, "senderId": 1, "recipientId": 420, "content": "parking spot X wants you to make space for him", "date": "04.20.2019, 16:20", "dismissed": false, "dismissedDate": ""}]'
+        );
+    }
+
+    renderNotifications(): Array<JSX.Element> {
+        let notifications: Array<JSX.Element> = [];
+        for (let i: number = 0; i < this.state.notifications.length; i++) {
+            const currentNotification: NotificationJson = this.state
+                .notifications[i];
+            notifications.push(
+                <div key={i}>
+                    <Notification
+                        id={currentNotification.id}
+                        senderId={currentNotification.senderId}
+                        recipientId={currentNotification.recipientId}
+                        content={currentNotification.content}
+                        date={currentNotification.date}
+                        dismissed={currentNotification.dismissed}
+                        dismissedDate={currentNotification.dismissedDate}
+                        dismissalHandler={this.dismissNotification}
+                    />
+                </div>
+            );
+        }
+        return notifications;
+    }
+
+    dismissNotification(id: number): void {}
+
+    render() {
+        return (
+            <div className={"notificationContainer"}>
+                <div>
+                    <div>
+                        <a>Notifications</a>
+                    </div>
+                    <div>
+                        <a>Archive</a>
+                    </div>
+                </div>
+                <hr />
+                <div>{this.renderNotifications()}</div>
+            </div>
+        );
+    }
+}
+
+export default NotificationContainer;
--- frontend/src/components/profile/Profile.tsx
+++ frontend/src/components/profile/Profile.tsx
@@ -1,24 +1,19 @@
 import React from "react";
-import Notification from "./Notification";
-import NotificationJson from "./NotificationJson";
+import NotificationContainer from "./NotificationContainer";
 
 class Profile extends React.Component<
     {},
     {
         currentUserId: number;
-        notifications: Array<NotificationJson>;
     }
 > {
     constructor(props: {}) {
         super(props);
 
         Profile.getCurrentUserId = Profile.getCurrentUserId.bind(this);
-        Profile.fetchNotifications = Profile.fetchNotifications.bind(this);
         this.state = {
-            currentUserId: Profile.getCurrentUserId(),
-            notifications: Profile.fetchNotifications()
+            currentUserId: Profile.getCurrentUserId()
         };
-        this.renderNotifications = this.renderNotifications.bind(this);
     }
 
     static getCurrentUserId(): number {
@@ -30,57 +25,12 @@ class Profile extends React.Component<
             : 0;
     }
 
-    static fetchNotifications(): Array<NotificationJson> {
-        /*
-        fetch(`http://localhost:8080/notificationsForUser/{this.props.currentUserId}`)
-            .then(result => result.json())
-            .then(notifications => this.setState({
-                notifications: JSON.parse(notifications)
-            }));
-        */
-        return JSON.parse(
-            '[{"id": 0, "senderId": 1, "recipientId": 420, "content": "parking spot X wants you to make space for him", "date": "04.20.2019, 16:20", "dismissed": false, "dismissedDate": ""}]'
-        );
-    }
-
-    renderNotifications(): Array<JSX.Element> {
-        let notifications: Array<JSX.Element> = [];
-        for (let i: number = 0; i < this.state.notifications.length; i++) {
-            const currentNotification: NotificationJson = this.state
-                .notifications[i];
-            notifications.push(
-                <div key={i}>
-                    <Notification
-                        id={currentNotification.id}
-                        senderId={currentNotification.senderId}
-                        recipientId={currentNotification.recipientId}
-                        content={currentNotification.content}
-                        date={currentNotification.date}
-                        dismissed={currentNotification.dismissed}
-                        dismissedDate={currentNotification.dismissedDate}
-                        dismissalHandler={this.dismissNotification}
-                    />
-                </div>
-            );
-        }
-        return notifications;
-    }
-
-    dismissNotification(id: number): void {}
-
     render() {
         return (
-            <div className={"notificationContainer"}>
-                <div>
-                    <div>
-                        <a>Notifications</a>
-                    </div>
-                    <div>
-                        <a>Archive</a>
-                    </div>
-                </div>
-                <hr />
-                <div>{this.renderNotifications()}</div>
+            <div>
+                <NotificationContainer
+                    currentUserId={this.state.currentUserId}
+                />
             </div>
         );
     }
